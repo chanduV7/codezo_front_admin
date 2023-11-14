9b40360b5886760ea0f362cd56ee1a393b6ddaef
chanduV7 <chandravarre818@gmail.com>
2023-11-14 13:27:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AdminLogin from '../src/components/admin/adminLogin';
+import DashBoard from '../src/components/admin/dashBoard';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BuildProfile from './components/buildProfile';
+import ProfileForm from './components/profileForm';
+import UploadPic from './components/uploadPic';
+
+
+
+function App() {
+   const router = createBrowserRouter([
+   
+  
+    { 
+      path:"/profile=/:userId",
+      element:<BuildProfile/>,
+    },
+   {
+     path: "/profile/edit/:userId",
+     element: <ProfileForm/>
+   },
+
+      {
+        path:"/admin/login",
+        element:<AdminLogin/>
+      },
+      {
+        path: "/admin/dashboard",
+        element: <DashBoard/>
+      },
+      {
+        path: "/profile/edit-pic",
+        element: <UploadPic/>
+      }
+     
+   ])
+  return (
+    <div className="App">
+             <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
